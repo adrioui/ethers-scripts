6241aea597b97fbae3adda24f9ac07dcfa499338
adrioui <adrifdlh@gmail.com>
2025-08-11 22:47:14 +0700

    feat(node): talk to our own node

---
+++ day3/providers.js
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import { JsonRpcProvider, formatEther, parseEther } from "ethers";
+
+const localUrl = `http://localhost:8545`;
+const localProvider = new JsonRpcProvider(localUrl);
+console.log(
+	"Current block number on Local Node:",
+	await localProvider.getBlockNumber(),
+);
+
+const myBalance = await localProvider.getBalance("0x94A5D03FEe64dADE55Df989fE2Cb3AA74c06b7ec");
+console.log(myBalance)
+// const sanfordBalance = await localProvider.getBalance("sanfordstout.eth");
