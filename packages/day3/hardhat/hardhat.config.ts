e1fed5a813a51233865fa6479dbf987242082703
adrioui <adrifdlh@gmail.com>
2025-08-14 23:06:09 +0700

    chore(network): add Sepolia network to test

--- packages/day3/hardhat/hardhat.config.ts
+++ packages/day3/hardhat/hardhat.config.ts
@@ -1,9 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
-	defaultNetwork: "localhost",
 	solidity: "0.8.28",
+	networks: {
+		sepolia: {
+			url: `https://sepolia.infura.io/v3/${process.env.INFURA_KEY}`,
+			accounts: [process.env.MY_WALLET_PRIVATE_KEY],
+		},
+	},
 };
 
 export default config;
